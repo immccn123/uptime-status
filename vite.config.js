cfe3a19f533b305cfccdef84e3d98b3eb18d9293
Imken Luo <me@imken.moe>
2023-09-20 10:07:19 +0800

    optimized css bundle size

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: "/",
   plugins: [react()],
   build: {
     cssMinify: "lightningcss",
     cssTarget: "chrome61",
-  }
-})
+    minify: "terser",
+    terserOptions: {
+      keep_fnames: false,
+      toplevel: true,
+    },
+  },
+});
