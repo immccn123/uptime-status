d9ced6e2b13a25f1dd06cbb906624be488e5d82a
LeterTW <i@moe.lv>
2024-08-20 16:22:30 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -7,6 +7,10 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
+    'm784488775-dd1ad84b209c05f8e185c33e',
+    'm784490063-7b5da437e7f1e0d67613714d',
+    'm784497419-de55aa09902ccb3ab22d548a',
+    'm784496436-71a4bf7b1e3bdf7756be131b',
     'm794118336-d8a3ecfeaf83115869bd85e6', // imken-blog
     'm795283182-5b9afc8984dd5d36cc731697', // Luogu Feed Saver
     'm797098613-a2720b05a17d5be73b32b7e9', // Luogu Benben Saver API
